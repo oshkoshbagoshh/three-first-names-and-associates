b1f8f6c24be5ce60b969b9fbf8f6699731c525f9
AJ Javadi <amirjavadi25@gmail.com>
2024-10-20 16:40:25 -0500

    new comps

---
+++ resources/js/Components/CalendlyWidget.vue
@@ -0,0 +1,42 @@
+<!-- CalendlyWidget.vue -->
+<template>
+    <div class="calendly-inline-widget" :data-url="calendlyUrl" :style="widgetStyle"></div>
+</template>
+
+<script>
+import { defineComponent, onMounted } from 'vue';
+
+export default defineComponent({
+    name: 'CalendlyWidget',
+    props: {
+        url: {
+            type: String,
+            default: 'https://calendly.com/ajavadi202/31min'
+        },
+        height: {
+            type: String,
+            default: '700px'
+        }
+    },
+    setup(props) {
+        const calendlyUrl = props.url;
+        const widgetStyle = {
+            minWidth: '320px',
+            height: props.height
+        };
+
+        onMounted(() => {
+            const script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = 'https://assets.calendly.com/assets/external/widget.js';
+            script.async = true;
+            document.head.appendChild(script);
+        });
+
+        return {
+            calendlyUrl,
+            widgetStyle
+        };
+    }
+});
+</script>
