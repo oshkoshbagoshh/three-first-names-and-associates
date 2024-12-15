75d9b1f3e28be26d338df3ef613ba3871b9664eb
oshkoshbagoshh <amirjavadi25@gmail.com>
2024-12-15 17:56:06 -0600

    updates to MainLayout

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,47 +1,47 @@
 <template>
-    <div class="h-full bg-white dark:bg-smoky-black text-smoky-black dark:text-white font-sans" :class="{ 'dark': darkMode }">
-<!--        Dark Mode Toggle Button-->
-        <button @click="toggleDarkMode" class="fixed top-4 right-4 z-50 p-2 bg-gray-200 dark:bg-gray-800 rounded-full">
-            <svg v-if="!darkMode" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
-            </svg>
-            <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
-            </svg>
-        </button>
+  <div class="h-full bg-white dark:bg-smoky-black text-smoky-black dark:text-white font-sans">
+    <!-- -->
 
 
-<!-- Navigation -->
-        <Header />
-        <!-- Navigation -->
-        <nav class="bg-gray-100 dark:bg-gray-800 py-4">
-            <div class="container mx-auto px-4 flex items-center justify-between">
-<!--                HOME LOGO -->
-                <LogoHome />
-                <ul class="flex justify-center space-x-6">
-                    <li><a @click.prevent="scrollToSection('about')" href="#about" class="text-indigo dark:text-blue-violet hover:text-sapphire dark:hover:text-sapphire">About</a></li>
-                    <li><a @click.prevent="scrollToSection('services')" href="#services" class="text-indigo dark:text-blue-violet hover:text-sapphire dark:hover:text-sapphire">Services</a></li>
-                    <li><a @click.prevent="scrollToSection('partnerships')" href="#partnerships" class="text-indigo dark:text-blue-violet hover:text-sapphire dark:hover:text-sapphire">Partnerships</a></li>
-                    <li><a @click.prevent="scrollToSection('music')" href="#music" class="text-indigo dark:text-blue-violet hover:text-sapphire dark:hover:text-sapphire">Music Services</a></li>
-                    <li><a @click.prevent="scrollToSection('contact')" href="#contact" class="text-indigo dark:text-blue-violet hover:text-sapphire dark:hover:text-sapphire">Contact</a></li>
-                </ul>
-            </div>
-        </nav>
+    <!-- Navigation -->
+    <Header/>
+    <!-- Navigation -->
+    <nav class="bg-gray-100 dark:bg-gray-800 py-4">
+      <div class="container mx-auto px-4 flex items-center justify-between">
+        <!--                HOME LOGO -->
+        <LogoHome/>
+        <ul class="flex justify-center space-x-6">
+          <li><a @click.prevent="scrollToSection('about')" href="#about"
+                 class="text-indigo dark:text-blue-violet hover:text-sapphire dark:hover:text-sapphire">About</a></li>
+          <li><a @click.prevent="scrollToSection('services')" href="#services"
+                 class="text-indigo dark:text-blue-violet hover:text-sapphire dark:hover:text-sapphire">Services</a>
+          </li>
+          <li><a @click.prevent="scrollToSection('partnerships')" href="#partnerships"
+                 class="text-indigo dark:text-blue-violet hover:text-sapphire dark:hover:text-sapphire">Partnerships</a>
+          </li>
+          <li><a @click.prevent="scrollToSection('music')" href="#music"
+                 class="text-indigo dark:text-blue-violet hover:text-sapphire dark:hover:text-sapphire">Music
+            Services</a></li>
+          <li><a @click.prevent="scrollToSection('contact')" href="#contact"
+                 class="text-indigo dark:text-blue-violet hover:text-sapphire dark:hover:text-sapphire">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
 
 
-        <!--        Main content -->
-        <slot></slot>
-<!--        -->
-        <Footer />
+    <!--        Main content -->
+    <slot></slot>
+    <!--        -->
+    <Footer/>
 
-        <ScrollToTop />
-        <!-- Dark Mode Toggle Button -->
-<!--        <button @click="toggleDarkMode" class="fixed top-4 right-4 z-50 p-2 bg-gray-200 dark:bg-gray-800 rounded-full"></button>-->
-    </div>
+    <ScrollToTop/>
+    <!-- Dark Mode Toggle Button -->
+    <!--        <button @click="toggleDarkMode" class="fixed top-4 right-4 z-50 p-2 bg-gray-200 dark:bg-gray-800 rounded-full"></button>-->
+  </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import {ref} from 'vue'
 import Header from '@/Components/Header.vue'
 import Footer from '@/Components/Footer.vue'
 import LogoHome from "@/Components/LogoHome.vue";
@@ -49,33 +49,33 @@ import ScrollToTop from "@/Components/ScrollToTop.vue";
 
 
 export default {
-    name: 'MainLayout',
-    components: {
-        ScrollToTop,
-        LogoHome,
-        Header,
-        Footer
-    },
-    setup() {
-        const darkMode = ref(false)
+  name: 'MainLayout',
+  components: {
+    ScrollToTop,
+    LogoHome,
+    Header,
+    Footer
+  },
+  setup() {
+    const darkMode = ref(false)
 
-        const toggleDarkMode = () => {
-            darkMode.value = !darkMode.value
-        }
-
-        const scrollToSection = (sectionId) => {
-            const element = document.getElementById(sectionId)
-            if (element) {
-                element.scrollIntoView({behavior: 'smooth'})
-            }
-
-        }
-
-        return {
-            darkMode,
-            toggleDarkMode,
-            scrollToSection
-        }
+    const toggleDarkMode = () => {
+      darkMode.value = !darkMode.value
     }
+
+    const scrollToSection = (sectionId) => {
+      const element = document.getElementById(sectionId)
+      if (element) {
+        element.scrollIntoView({behavior: 'smooth'})
+      }
+
+    }
+
+    return {
+      darkMode,
+      toggleDarkMode,
+      scrollToSection
+    }
+  }
 }
 </script>
