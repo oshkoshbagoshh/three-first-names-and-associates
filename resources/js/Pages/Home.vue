2aaaadc7b6fc27698125e873396e1d94c70052f2
oshkoshbagoshh <amirjavadi25@gmail.com>
2025-01-11 07:25:12 -0600

    feat: update MainLayout and Home sections with improved structure and content

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -58,13 +58,35 @@
         <section id="partnerships" class="mb-8">
             <!-- TODO: partnerships  -->
             <h2 class="mb-4 text-2xl font-semibold font-rubik text-indigo dark:text-blue-violet">Partnerships</h2>
-            <ul>
-                <li>Partner 1 </li>
-                <li>Partner 2 </li>
-                <li>Partner 3 </li>
-            </ul>
+
+            <p> Perspiciatis quidem veniam eum necessitatibus aut fugiat deserunt dolores. Inventore magni aut rerum
+                perspiciatis consectetur et. Aut omnis eius voluptatem dolores. Occaecati culpa aut pariatur
+                sapiente. Ex est ea molestiae dolor ut sunt ipsam.</p>
+
+            <p>
+
+                Cumque vel recusandae modi repellat maxime suscipit. Fugit reprehenderit accusantium aliquam dolorum
+                voluptates temporibus voluptas saepe ea. Eius rerum tempora reprehenderit dolor sint consequatur quo
+                ipsam qui. Laboriosam dignissimos adipisci consequatur ducimus ratione culpa eligendi.
+
+                Dicta et laboriosam est amet facilis ut sunt occaecati aspernatur. Ea rerum eos consequuntur. Sed
+                laborum quod laudantium suscipit. Eos vero voluptatem ad vero incidunt fugiat consequatur excepturi.
+                Facere officiis aliquid placeat necessitatibus debitis esse quo. Quia voluptas autem ea ut non
+                aspernatur quo consequuntur vel.
+
+                A et voluptates sed. Facere voluptatum expedita repellat rem aut nesciunt distinctio repudiandae. Et sit
+                recusandae quo explicabo culpa placeat. Autem qui eos odio vitae minus neque minima quas odio. Quibusdam
+                doloribus odit sit fuga neque libero sed hic ut. Nihil autem explicabo hic asperiores rerum aut.
+            </p>
+
+
+            <li>Partner 1 </li>
+            <li>Partner 2 </li>
+            <li>Partner 3 </li>
+
 
         </section>
+
 
 
 
