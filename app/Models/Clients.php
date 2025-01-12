88970918b53db0487653fb26514491bb5b7a4b86
oshkoshbagoshh <amirjavadi25@gmail.com>
2025-01-12 16:35:11 -0600

    feat: add Clients model with mass assignable attributes and factory support

---
+++ app/Models/Clients.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * @Author: AJ Javadi
+ * @Email: amirjavadi25@gmail.com
+ * @Date: 2025-01-11 23:19:18
+ * @Last Modified by:   Someone
+ * @Last Modified time: 2025-01-11 23:23:03
+ * @Description: file:///Users/aj/Herd/tresllamos/app/Models/Clients.php
+ */
+
+
+ //==============================================================================
+
+
+
+/**
+ *
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Clients extends Model
+{
+    /** @use HasFactory<\Database\Factories\ClientFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+     // for now, i'm just going to use data.json with dummy data for prospective clients
+
+
+}
